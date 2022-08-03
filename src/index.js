0b8d3093fccdfb9f311bb4371d30235f5f2caadb
Dmitrii Ushakov <81690819+DimkaUsh@users.noreply.github.com>
2022-08-03 16:40:15 +0800

    feat: add normalize style

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-// import './index.css';
+import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
